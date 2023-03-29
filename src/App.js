d72f15960bc770ac5203b2aa2622d07086189568
Aila <ma185515@ncr.com>
2023-03-29 04:48:54 -0400

    data.json edited

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Dashboard from './Dashboard';
 import Home from './Home';
-function App() {
 
+function App() {
+  const [data1,setdata] = useState([]);
+  const getData = ()=>{
+    fetch(`data.json`, {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      setdata(data)
+      console.log(data1);
+    });  
+  }
+  useEffect(()=>{
+    getData()
+  },[])
   return (
     <>
       <BrowserRouter>
@@ -15,4 +33,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
