70f835c759d6c961865d551e28f8bcba44388666
Aila <ma185515@ncr.com>
2023-04-04 01:27:12 -0400

    created build

---
+++ src/servergrid.js
@@ -0,0 +1,22 @@
+export const servergrid = [
+    {
+      field: 'OrderItems',
+      headerText: 'Item',
+      width: '150',
+      editType: 'dropdownedit',
+      textAlign: 'Center',
+    },
+    { field: 'CustomerName',
+      headerText: 'Customer Name',
+      width: '150',
+      textAlign: 'Center',
+    },
+    {
+      field: 'TotalAmount',
+      headerText: 'Total Amount',
+      format: 'C2',
+      textAlign: 'Center',
+      editType: 'numericedit',
+      width: '150',
+    }
+  ];
